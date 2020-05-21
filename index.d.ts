59e6b002821060c88ecbd0d623bc7c0d2b38382d
Robert Greene <robert@zardozcs.com>
2020-05-21 03:42:21 -0700

    fix types

--- index.d.ts
+++ index.d.ts
@@ -1,26 +1,22 @@
-declare module 'react-native-radial-context-menu' {
-	export interface IButton {
-		value: any;
-		component(): React.ReactNode;
-	}
-
-	export interface withRadialActionMenuProps {
-		backgroundColor?: string;
-		buttonDiameter?: number;
-		buttons: IButton[];
-		menuDiameter?: number;
-		openDelay?: number;
-		spreadAngle?: number;
-		spreadRadius?: number;
-		vibrate?: boolean;
-		vibrateDuration?: number;
-		onClose?(value: IButton["value"]): void;
-		onOpen?(): void;
-	}
-
-	export type withRadialActionMenu = <P extends withRadialActionMenuProps>(
-		// tslint:disable-next-line:variable-name
-		Component: React.ComponentType<P>
-	) => React.ComponentClass
-
+export interface IButton {
+	value: any;
+	component(): React.ReactNode;
 }
+
+export interface withRadialActionMenuProps {
+	backgroundColor?: string;
+	buttonDiameter?: number;
+	buttons: IButton[];
+	menuDiameter?: number;
+	openDelay?: number;
+	spreadAngle?: number;
+	spreadRadius?: number;
+	vibrate?: boolean;
+	vibrateDuration?: number;
+	onClose?(value: IButton["value"]): void;
+	onOpen?(): void;
+}
+
+export const withRadialActionMenu = <P extends withRadialActionMenuProps>(
+	Component: React.ComponentType<P>
+) => React.ComponentClass
